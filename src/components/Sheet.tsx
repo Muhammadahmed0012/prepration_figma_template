7c296cd16fff1cfa7234738beb6c1a3539966fbf
Muhammadahmed0012 <159796230+Muhammadahmed0012@users.noreply.github.com>
2024-12-07 15:43:04 +0500

    first commit

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -20,10 +20,10 @@ import {
           <SheetTitle></SheetTitle>
           <SheetDescription>
             <ul className="sm:flex sm:flex-col sm:items-center sm:text-xl sm:font-extralight sm:mt-14 space-y-10 flex flex-col items-center text-xl font-extralight mt-14 text-white">
-         <li><a href="">Home</a></li> 
-          <li><a href="">contact</a></li> 
-          <li><a href="">About</a></li> 
-          <li><a href="">Sign Up</a></li>
+         <li><a href="/">Home</a></li> 
+          <li><a href="/contact">contact</a></li> 
+          <li><a href="/about">About</a></li> 
+          <li><a href="/sign-up">Sign Up</a></li>
           <div className='sm:flex sm:items-center flex items-center'>
            <input type="search" placeholder='What are you looking for?'  className='sm:flex sm:items-center sm:pl-2 lg:text-sm sm:font-semibold sm:w-60 sm:h-8 sm:rounded-l-md flex items-center pl-2 text-sm font-semibold w-56 h-8 rounded-l-md  bg-[#E5E7EB]'/>
            <Search className='sm:h-8 sm:w-7 bg-[#E5E7EB] text-black rounded-r-md h-8 w-7 '/>
